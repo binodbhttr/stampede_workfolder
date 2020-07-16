8d116e4361b86572ed670b69ae8182fbe772cd2e
binodbhttr <binod.bhattarai@hotmail.com>
2020-07-16 00:42:25 -0500

    Added code to compare the ids of two snapshots

--- part2.py
+++ part2.py
@@ -60,5 +60,38 @@ ax2.set_title('in 2D (x vs y)')
 #plt.tight_layout()
 fig7.savefig("./plots/spherical_cut_1_young1_age7to8inlog10scale.png")
 
-ind_tracked_id_691=np.where(id_691==id_young1_d_xyz)
+
+
+###Now let's find the matching ids in the previous snapshot
+
+ind=np.array(0) #Creates an array ind with an element 0
+for i in range(len(id_young1_d_xyz)):
+  match=np.where(id_691==id_young1_d_xyz[i] #find each matching id and store its index in match
+  ind=np.append(ind,match) # the index of matching id is appended. But it has one extra element in the beginning
+  
+ind_tracked_id_691=ind[1:len(ind)] #The extra element in the beginning is removed by this process
 tracked_id_691=id_691[ind_tracked_id_691]
+
+x_691_tracked=x_691[ind_tracked_id_691]
+y_691_tracked=y_691[ind_tracked_id_691]
+z_691_tracked=z_691[ind_tracked_id_691]
+
+
+#Now plotting ind 2D the stars in two snapshots
+fig8 = plt.figure()
+ax1 = fig8.add_subplot(211, projection='3d')
+ax1.scatter(y_691_tracked,x_691_tracked,marker=".",s=0.5)
+ax1.set_xlabel('y')
+ax1.set_ylabel('x')
+ax1.set_title('Snapshot 691: young stars age 7 to 8 dec within 1kpc sphere from 0,8,0')
+
+plt.subplots_adjust(hspace=.5)
+
+ax2=fig7.add_subplot(212)
+ax2.scatter(y_young1_d_xyz,x_young1_d_xyz,marker=".",s=0.5)
+ax2.set_xlabel('y')
+ax2.set_ylabel('x')
+ax2.minorticks_on()
+ax2.set_title('Snapshot 696: young stars age 7 to 8 dec within 1kpc sphere from 0,8,0')
+#plt.tight_layout()
+fig7.savefig("./plots/young1_age7to8_snapshots691_and_696.png")
