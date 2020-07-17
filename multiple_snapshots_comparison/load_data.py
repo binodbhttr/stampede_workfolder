d3eeea6800ec2ffd779efa95af4b28ec119622ea
binodbhttr <binod.bhattarai@hotmail.com>
2020-07-16 22:12:14 -0500

    Created function to track multiple snapshots

--- multiple_snapshots_comparison/load_data.py
+++ multiple_snapshots_comparison/load_data.py
@@ -17,84 +17,159 @@ from mpl_toolkits.mplot3d import Axes3D
 
 simname = 'm12i_res7100_mhdcv'
 simdir = '/scratch/projects/xsede/GalaxiesOnFIRE/mhdcv/m12i_res7100_mhdcv/1Myr/'
-snapnumber = 696
+snapnumber = 691
 
 
 # In[ ]:
 
 
-part = gizmo.io.Read.read_snapshots(['star'],'snapshot_index', snapnumber, simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
+part_691 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index', snapnumber, simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True) #691 is the snapnumber here
 
-part_691 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index',691 , simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
-# In[ ]:
+part_692 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index',692 , simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
 
+part_693 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index',693 , simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
 
-part['star'].keys()
-id=part['star'].prop('id')
-id_child=part['star'].prop('id.child')
-print("Loaded the ids and id.child of the latest snapshot (696)",id,id_child)
+part_694 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index',694 , simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
 
+part_695 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index',695 , simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
+
+part_696 = gizmo.io.Read.read_snapshots(['star'],'snapshot_index',696 , simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)
+
+# Loading age and id data from snapshot 691
 part_691['star'].keys()
 id_691=part_691['star'].prop('id')
 id_child_691=part_691['star'].prop('id.child')
-print("Loaded the ids and id.child of the early snapshot (691)",id_691,id_child_691)
-
-# In[ ]:
-
-
-# 3-D position of star particle (particle number x dimension number) [kpc comoving]
-
-starposition=part['star']['position'] # starposition is the array of position of all stars
-ageall=part['star'].prop('age') #stored all ages in a
-
-starposition_691=part_691['star']['position']
+print(" \n Loaded the ids and id.child of the early snapshot (691)",id_691,id_child_691)
 ageall_691=part_691['star'].prop('age')
 
 
- 
-xall = part['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
-yall = part['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
-zall = part['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
-nall=len(xall) # counting the total no. of star particles
 
 
+
+
+# Loading age and id data from snapshot 692
+part_692['star'].keys()
+id_692=part_692['star'].prop('id')
+id_child_692=part_692['star'].prop('id.child')
+print(" \n Loaded the ids and id.child of the latest snapshot (692)",id_692,id_child_692)
+ageall_692=part_692['star'].prop('age')
+
+# Loading age and id data from snapshot 693
+part_693['star'].keys()
+id_693=part_693['star'].prop('id')
+id_child_693=part_693['star'].prop('id.child')
+print(" \n Loaded the ids and id.child of the latest snapshot (693)",id_693,id_child_693)
+ageall_693=part_693['star'].prop('age')
+
+
+# Loading age and id data from snapshot 694
+part_694['star'].keys()
+id_694=part_694['star'].prop('id')
+id_child_694=part_694['star'].prop('id.child')
+print(" \n Loaded the ids and id.child of the latest snapshot (694)",id_694,id_child_694)
+ageall_694=part_694['star'].prop('age')
+
+
+# Loading data from snapshot 695
+part_695['star'].keys()
+id_695=part_695['star'].prop('id')
+id_child_695=part_695['star'].prop('id.child')
+print(" \n Loaded the ids and id.child of the latest snapshot (695)",id_695,id_child_695)
+ageall_695=part_695['star'].prop('age')
+
+
+# Loading age and id data from snapshot 696
+part_696['star'].keys()
+id_696=part_696['star'].prop('id')
+id_child_696=part_696['star'].prop('id.child')
+print(" \n Loaded the ids and id.child of the latest snapshot (696)",id_696,id_child_696)
+ageall_696=part_696['star'].prop('age') 
+
+
+
+###################################################
 xall_691 = part_691['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
 yall_691 = part_691['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
 zall_691 = part_691['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
 nall_691=len(xall_691) # counting the total no. of star particles
-
-
-
-n=nall # This is the no. of stars we are going to use. n=nall means choose all, n=100 means choose 100
 n_691=nall_691
-print("Total particles in 696 snapshots is",n)
-print("Total particles in 691 snapshots is",n_691)                
-smallpart=starposition[0:n] # This is selecting a small part from the collection of all star positions
-
-x=xall[0:n] # select x components from 0 to n
-y=yall[0:n] # select y components from 0 to n
-z=zall[0:n] # select z components from 0 to n
-age=ageall[0:n]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
-
+print("\n Total particles in 691 snapshots is",n_691) 
 x_691=xall_691[0:n_691] # select x components from 0 to n
 y_691=yall_691[0:n_691] # select y components from 0 to n
 z_691=zall_691[0:n_691] # select z components from 0 to n
 age_691=ageall_691[0:n_691]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
+             
+############################
+###################################################
+xall_692 = part_692['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
+yall_692 = part_692['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
+zall_692 = part_692['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
+nall_692=len(xall_692) # counting the total no. of star particles
+n_692=nall_692
+print("\n Total particles in 692 snapshots is",n_692) 
+x_692=xall_692[0:n_692] # select x components from 0 to n
+y_692=yall_692[0:n_692] # select y components from 0 to n
+z_692=zall_692[0:n_692] # select z components from 0 to n
+age_692=ageall_692[0:n_692]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
+             
+############################
+###################################################
+xall_693 = part_693['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
+yall_693 = part_693['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
+zall_693 = part_693['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
+nall_693=len(xall_693) # counting the total no. of star particles
+n_693=nall_693
+print("\n Total particles in 693 snapshots is",n_693) 
+x_693=xall_693[0:n_693] # select x components from 0 to n
+y_693=yall_693[0:n_693] # select y components from 0 to n
+z_693=zall_693[0:n_693] # select z components from 0 to n
+age_693=ageall_693[0:n_693]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
+             
+############################
+###################################################
+xall_694 = part_694['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
+yall_694 = part_694['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
+zall_694 = part_694['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
+nall_694=len(xall_694) # counting the total no. of star particles
+n_694=nall_694
+print("\n Total particles in 694 snapshots is",n_694) 
+x_694=xall_694[0:n_694] # select x components from 0 to n
+y_694=yall_694[0:n_694] # select y components from 0 to n
+z_694=zall_694[0:n_694] # select z components from 0 to n
+age_694=ageall_694[0:n_694]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
+             
+############################
+###################################################
+xall_695 = part_695['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
+yall_695 = part_695['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
+zall_695 = part_695['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
+nall_695=len(xall_695) # counting the total no. of star particles
+n_695=nall_695
+print("\n Total particles in 695 snapshots is",n_695) 
+x_695=xall_695[0:n_695] # select x components from 0 to n
+y_695=yall_695[0:n_695] # select y components from 0 to n
+z_695=zall_695[0:n_695] # select z components from 0 to n
+age_695=ageall_695[0:n_695]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
+             
+############################
+###################################################
+xall_696 = part_696['star'].prop('host.distance.principal')[:,0] #x component of the position of all stars 
+yall_696 = part_696['star'].prop('host.distance.principal')[:,1] #y component of the position of all stars
+zall_696 = part_696['star'].prop('host.distance.principal')[:,2] #z component of the position of all stars
+nall_696=len(xall_696) # counting the total no. of star particles
+n_696=nall_696
+print("\n Total particles in 696 snapshots is",n_696) 
+x_696=xall_696[0:n_696] # select x components from 0 to n
+y_696=yall_696[0:n_696] # select y components from 0 to n
+z_696=zall_696[0:n_696] # select z components from 0 to n
+age_696=ageall_696[0:n_696]*1e9 #age converted to years Note: NOT IN GYR ANYMORE !!!!!!!!!!!
+             
+############################
 
-
-R=np.sqrt(np.square(x)+np.square(y)) #calculate the radius in the xy plane
-print("Calculated the cylindrical radius R in xy plane for the snapshot 696",R)
 
 
 R_691=np.sqrt(np.square(x_691)+np.square(y_691)) #calculate the radius in the xy plane
-print("Calculated the cylindrical radius R in xy plane for the snapshot 691",R_691)
-
-
-# In[ ]:
-
-
-
-# In[ ]:
+print("\n ################## \n Calculated the cylindrical radius R in xy plane for the snapshot 691",R_691)
 
 
 
--- multiple_snapshots_comparison/selections.py
+++ multiple_snapshots_comparison/selections.py
@@ -1,24 +1,24 @@
 from __main__ import *
 print("Now running the part to make selections from a different file !!!")
 
-ind_R7to9=np.where((R>7) & (R<9)) #Select those stars with r greater than 7 and less than 9
+ind_R7to9=np.where((R_691>7) & (R_691<9)) #Select those stars with r greater than 7 and less than 9
 
-x_R7to9=x[ind_R7to9] #find the x components only of the slection
-y_R7to9=y[ind_R7to9] #find the y components only of the slection
-z_R7to9=z[ind_R7to9]
-age_R7to9=np.log10(age[ind_R7to9])
-print("Note ages are already in log scale now !!!!! These are the ages from the snapshot (696) ",age_R7to9)
-R_R7to9=R[ind_R7to9] #store the cylindrical radius in array Note: the values should be between 7 and 9
-id_R7to9=id[ind_R7to9]
-id_child_R7to9=id_child[ind_R7to9]
+x_R7to9=x_691[ind_R7to9] #find the x components only of the slection
+y_R7to9=y_691[ind_R7to9] #find the y components only of the slection
+z_R7to9=z_691[ind_R7to9]
+age_R7to9=np.log10(age_691[ind_R7to9])
+print("\n Note ages are already in log scale now !!!!! These are the ages from the snapshot (696) ",age_R7to9)
+R_R7to9=R_691[ind_R7to9] #store the cylindrical radius in array Note: the values should be between 7 and 9
+id_R7to9=id_691[ind_R7to9]
+id_child_R7to9=id_child_691[ind_R7to9]
 
 
 d_xyz=np.sqrt(x_R7to9**2+(y_R7to9-8)**2+z_R7to9**2)   #calculate radius of all stars in xyz plane form a point 
-print("Calculated the spherical radius for all points that lie inside the cylindrical radius 7 and 9") #inside the selection we made previously. Here the point is (0,8)
-ind_d_xyz_lessthan1=np.where(d_xyz<1)
-print("Selected the indices of those objects that are within 1kpc spherical radius")
+print(" \n Calculated the spherical radius for all points that lie inside the cylindrical radius 7 and 9") #inside the selection we made previously. Here the point is (0,8)
+ind_d_xyz_lessthan1=np.where((d_xyz>0)&(d_xyz<1))
+print("\n Selected the indices of those objects that are within 1 kpc spherical radius",ind_d_xyz_lessthan1)
 d_xyz_lessthan1=d_xyz[ind_d_xyz_lessthan1] #here we stored those distances that are less than 1 in xyz plane
-print("Total objects in that radius is",len(d_xyz_lessthan1)) #we found 971 elements that satisfy our condition
+print("\n Total objects in that radius is",len(d_xyz_lessthan1)) #count the no. of such stars
 
 
 x_d_xyz_lessthan1=x_R7to9[ind_d_xyz_lessthan1] 
@@ -62,76 +62,119 @@ ax2.set_ylabel('x')
 ax2.minorticks_on()
 ax2.set_title('in 2D (x vs y)')
 #plt.tight_layout()
-fig7.savefig("./plots/spherical_cut_1_young1_age7to8inlog10scale.png")
+fig7.savefig("./plots/verytiny_cut_1_young1_age7to8inlog10scale.png")
 
 
 
-###Now let's find the matching ids in the previous snapshot
-
-ind=np.array(0) #Creates an array ind with an element 0
-for i in range(len(id_young1_d_xyz)):
-  match=np.where((id_691==id_young1_d_xyz[i])&(id_child_691==id_child_young1_d_xyz[i])) #find each matching id and store its index in match
-  print("Matched the id",id_691[match])
-  ind=np.append(ind,match) # the index of matching id is appended. But it has one extra element in the beginning
-  
-print(ind)
-ind_tracked_id_691=ind[1:len(ind)] #The extra element in the beginning is removed by this process
-tracked_id_691=id_691[ind_tracked_id_691]
-
-print("The number of stars in Snapshot 691 is",len(tracked_id_691))
-print("Which is strange as they are not equal. I think 691 could have a little less but not more !!!!!!!!!!!! ") # There are duplicated IDss !!!
-print("Total ids in Snapshot 696 was",len(id))
-print("Total ids in Snapshot 691 was",len(id_691))
+###Now let's find the matching ids in the next snapshot using a function
+def matchids(id_current,id_child_current,id_next,id_child_next):  #this function returns the index of the ids in the next snapshot that match with the current
+  ind=np.array(0)
+  for i in range(len(id_current)):
+    match=np.where((id_next==id_current[i])&(id_child_next==id_child_current[i]))
+    print("\nMatched the id",id_next[match])
+    ind=np.append(ind,match)
+  ind_tracked_id_next=ind[1:len(ind)]  #The extra element in the beginning is removed by this process
+  print("\nThese are the indices of the ids that matched in current snapshot\n",ind_tracked_id_next)
+  return ind_tracked_id_next  
 
 
-x_691_tracked=x_691[ind_tracked_id_691]
-y_691_tracked=y_691[ind_tracked_id_691]
-z_691_tracked=z_691[ind_tracked_id_691]
+ind_tracked_id_692=matchids(id_young1_d_xyz,id_child_young1_d_xyz,id_692,id_child_692)
+ind_tracked_id_693=matchids(id_young1_d_xyz,id_child_young1_d_xyz,id_693,id_child_693)
+ind_tracked_id_694=matchids(id_young1_d_xyz,id_child_young1_d_xyz,id_694,id_child_694)
+ind_tracked_id_695=matchids(id_young1_d_xyz,id_child_young1_d_xyz,id_695,id_child_695)
+ind_tracked_id_696=matchids(id_young1_d_xyz,id_child_young1_d_xyz,id_696,id_child_696)
+
+
+
+
+x_692_tracked=x_692[ind_tracked_id_692]
+y_692_tracked=y_692[ind_tracked_id_692]
+z_692_tracked=z_692[ind_tracked_id_692]
+ 
+#########
+x_693_tracked=x_693[ind_tracked_id_693]
+y_693_tracked=y_693[ind_tracked_id_693]
+z_693_tracked=z_693[ind_tracked_id_693]
+
+#########
+x_694_tracked=x_694[ind_tracked_id_694]
+y_694_tracked=y_694[ind_tracked_id_694]
+z_694_tracked=z_694[ind_tracked_id_694]
+
+#########
+x_695_tracked=x_695[ind_tracked_id_695]
+y_695_tracked=y_695[ind_tracked_id_695]
+z_695_tracked=z_695[ind_tracked_id_695]
+
+#########
+x_696_tracked=x_696[ind_tracked_id_696]
+y_696_tracked=y_696[ind_tracked_id_696]
+z_696_tracked=z_696[ind_tracked_id_696]
+
 
 
 #Now plotting ind 2D the stars in two snapshots
 fig8 = plt.figure()
-ax1 = fig8.add_subplot(211)
-ax1.scatter(y_691_tracked,x_691_tracked,marker=".",s=0.5)
+#fig8.suptitle("Stars aged 7 to 8 dec within 1 from 0,8,0 \n \n")
+ax1 = fig8.add_subplot(321)
+ax1.scatter(y_young1_d_xyz,x_young1_d_xyz,marker=".",s=0.5)
 ax1.set_xlabel('y')
 ax1.set_ylabel('x')
-ax1.set_title('Snapshot 691: young stars age 7 to 8 dec within 1kpc sphere from 0,8,0')
-
-plt.subplots_adjust(hspace=.5)
-
-ax2=fig8.add_subplot(212)
-ax2.scatter(y_young1_d_xyz,x_young1_d_xyz,marker=".",s=0.5)
-ax2.set_xlabel('y')
-ax2.set_ylabel('x')
-ax2.minorticks_on()
-
-ax2.set_title('Snapshot 696: young stars age 7 to 8 dec within 1kpc sphere from 0,8,0')
-#plt.tight_layout()
-fig8.savefig("./plots/young1_age7to8_snapshots691_and_696.png")
-
-
-
-
-
-#Now plotting figure 08 with y limits and x limits
-fig9 = plt.figure()
-ax1 = fig9.add_subplot(211)
-ax1.scatter(y_691_tracked,x_691_tracked,marker=".",s=0.5)
-ax1.set_xlabel('y')
-ax1.set_ylabel('x')
+ax1.minorticks_on()
 ax1.set_ylim(-2,2)
 ax1.set_xlim(-10,10)
-ax1.set_title('Snapshot 691: young stars age 7 to 8 dec within 1kpc sphere from 0,8,0')
+ax1.set_title('Snapshot 691')
 
-plt.subplots_adjust(hspace=.5)
-
-ax2=fig9.add_subplot(212)
-ax2.scatter(y_young1_d_xyz,x_young1_d_xyz,marker=".",s=0.5)
+ax2=fig8.add_subplot(322)
+ax2.scatter(y_692_tracked,x_692_tracked,marker=".",s=0.5)
 ax2.set_xlabel('y')
 ax2.set_ylabel('x')
 ax2.minorticks_on()
 ax2.set_ylim(-2,2)
 ax2.set_xlim(-10,10)
-ax2.set_title('Snapshot 696: young stars age 7 to 8 dec within 1kpc sphere from 0,8,0')
-#plt.tight_layout()
-fig9.savefig("./plots/same_scale_young1_age7to8_snapshots691_and_696.png")
+ax2.set_title('Snapshot 692')
+
+
+ax3=fig8.add_subplot(323)
+ax3.scatter(y_693_tracked,x_693_tracked,marker=".",s=0.5)
+ax3.set_xlabel('y')
+ax3.set_ylabel('x')
+ax3.set_ylim(-2,2)
+ax3.set_xlim(-10,10)
+ax3.minorticks_on()
+ax3.set_title('Snapshot 693')
+
+#plt.subplots_adjust(hspace=.5)
+
+ax4=fig8.add_subplot(324)
+ax4.scatter(y_694_tracked,x_694_tracked,marker=".",s=0.5)
+ax4.set_xlabel('y')
+ax4.set_ylabel('x')
+ax4.minorticks_on()
+ax4.set_ylim(-2,2)
+ax4.set_xlim(-10,10)
+ax4.set_title('Snapshot 694')
+
+
+ax5=fig8.add_subplot(325)
+ax5.scatter(y_695_tracked,x_695_tracked,marker=".",s=0.5)
+ax5.set_xlabel('y')
+ax5.set_ylabel('x')
+ax5.minorticks_on()
+ax5.set_ylim(-2,2)
+ax5.set_xlim(-10,10)
+ax5.set_title('Snapshot 695')
+
+#plt.subplots_adjust(hspace=.5)
+
+ax6=fig8.add_subplot(326)
+ax6.scatter(y_696_tracked,x_696_tracked,marker=".",s=0.5)
+ax6.set_xlabel('y')
+ax6.set_ylabel('x')
+ax6.minorticks_on()
+ax6.set_ylim(-2,2)
+ax6.set_xlim(-10,10)
+ax6.set_title('Snapshot 696')
+
+plt.tight_layout()
+fig8.savefig("./plots/young1_age7to8_snapshots691to696.png")
