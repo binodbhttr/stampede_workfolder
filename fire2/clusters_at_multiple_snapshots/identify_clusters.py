277ca0f850f14dbd1c5b9c20b5704ba43261f1d9
binodbhttr <binod.bhattarai@hotmail.com>
2021-03-30 14:15:48 -0500

    added analysis of clusters from multiple snapshots tracked until 30 Myrs

--- fire2/clusters_at_multiple_snapshots/identify_clusters.py
+++ fire2/clusters_at_multiple_snapshots/identify_clusters.py
@@ -1,13 +1,33 @@
+import matplotlib as plt2 #need this for patches for shaded circles
 import numpy as np
-import gizmo_analysis as gizmo
+import matplotlib.pyplot as plt
+import gizmo_analysis as gizmo 
 import utilities as ut
-from fof_analysis import fof
+import matplotlib.colors as colors
+from matplotlib import rc #to use Latex math symbols like 'phi'
+import astropy
+from astropy.io import ascii
+import matplotlib
+import pdb
+from importlib import reload
+from sl_utilities import distance_functions
+#pdb.set_trace()  #<--in case need to troubleshoot
 import pickle
 import os
-simname = 'm12i_res7100_mhdcv'
-simdir = '/scratch/projects/xsede/GalaxiesOnFIRE/mhdcv/m12i_res7100_mhdcv/1Myr/fire3'
-snapnumber = 620
+from fof_analysis import fof
 
+
+#########################################################################
+#########################################################################
+#load particle data here as you normally do to get the star information
+#cut on Rxy, |z| and age
+#run fof
+#sind, sxcm, sycm, szcm, smtot, sgrpid, sr90, sr50, srmax =fof.find(x[si],y[si],z[si], b=b_kpc, mass=mass[si], ncut=ncut_min)
+#srcm = np.sqrt(sxcm**2. + sycm**2.)
+
+simname = 'm12i_res7100_mhdcv'
+simdir = '/scratch/projects/xsede/GalaxiesOnFIRE/cr_suite/m12i_res7100/mhdcv/1Myr/fire2/'
+snapnumber = 608
 part = gizmo.io.Read.read_snapshots(['all'],'snapshot_index', snapnumber, simulation_name=simname, simulation_directory=simdir, assign_hosts_rotation=True, assign_hosts=True)  
 
 rxyz     = part['star'].prop('host.distance.total')
@@ -20,10 +40,8 @@ feh      = part['star'].prop('metallicity.fe')
 ids      = part['star'].prop('id')
 id_child = part['star'].prop('id.child')
 age      = part['star'].prop('age')
-
 #select young stars within the disk for star clusters
-keep = np.where((age <= .003) & (rxyz < 20) & (abs(z) < 1.5))
-
+keep = np.where((age <= .003) & ((Rxy < 20) & (Rxy>2)) & (abs(z) < 1.5))
 #to run cluster finding on
 rxyz0     = rxyz[keep]
 Rxy0      = Rxy[keep]
@@ -36,8 +54,8 @@ id0       = ids[keep]
 id_child0 = id_child[keep]
 age0      = age[keep]
 
-linking_length = 0.01 #10 parsec
-ncut           = 10 #10 star particles
+linking_length = 0.004 #4 parsec
+ncut           = 4 #4 star particles
 
 ind, xcm, ycm, zcm, mtot, grpid, r90, r50, rmax =fof.find(x0,y0,z0, b=linking_length, mass=mass0, ncut=ncut)
 ngroup = len(mtot)
@@ -47,12 +65,13 @@ for grp_index in range(ngroup):  #iterate over each group
     cluster={}
     ids_in_cluster = id0[ind[grp_index]]  #these are the star particle ids in each cluster
     id_children_in_cluster = id_child0[ind[grp_index]]
+    age=age0[ind[grp_index]]
     nstar = len(ids_in_cluster)
     groupid=grpid[grp_index]
     print('------------------------------------------------------------------------------------------------------------------')
     print('grpid, nstar, xcm (kpc), ycm (kpc), zcm (kpc), mtot (msun), rmax (pc)')
     print('%s     %i     %.4f     %.4f    %.4f     %.2e     %.1f ' % (grpid[grp_index], nstar, xcm[grp_index], ycm[grp_index], zcm[grp_index], mtot[grp_index], 1000*rmax[grp_index]))
-   
+    print("age os stars in the cluster",age)
     print('ids')
     string = '[' 
     for i in ids_in_cluster:
@@ -72,16 +91,23 @@ for grp_index in range(ngroup):  #iterate over each group
     length = len(string)-2
     string = string[0:length] + ']'
     print(string)
-    cluster={"cluster_groupid":groupid,"no_of_star":nstar,"id":ids_in_cluster,"id_children":id_children_in_cluster}
+    print("These are the ids printed",ids_in_cluster)
+    #feh_in_cluster=feh[ids_in_cluster]
+    cluster={"cluster_groupid":groupid,"no_of_star":nstar,"id":ids_in_cluster,"id_children":id_children_in_cluster,"xcm":xcm[grp_index],"ycm":ycm[grp_index],"zcm":zcm[grp_index],"mtot":mtot[grp_index],"r90":r90[grp_index],"r50":r50[grp_index],"rmax":rmax[grp_index],"x":x0[ind[grp_index]],"y":y0[ind[grp_index]],"z":z0[ind[grp_index]],"age":age0[ind[grp_index]]}
     export_cluster.update({groupid:cluster})
+
+
+
+######################################################################
+#######################################################################
 
 print('------------------------------------------------------------------------------------------------------------------')
 
-path="./fire3data_pkl/" #creating a path to store the data only if it does not exist
+path="./fire2_data_pkl/" #creating a path to store the data only if it does not exist
 if not os.path.exists(path):
   os.makedirs(path)
 
-file_name="fire3_clusters_"+simname+"_snapshot_"+str(snapnumber) 
+file_name="fire2_clusters_"+simname+"_snapshot_"+str(snapnumber)+".pkl" 
 
 with open(path+file_name, 'wb') as output:
     # Pickle dictionary using protocol 0.
@@ -96,3 +122,9 @@ print("\n\n\nThe ids of the cluster with group id 1 is",import_cluster[1]["id"])
 ########################
 print("Total clusters present is",len(import_cluster))
 
+
+###################################
+####################################
+#####################################
+#######################################
+#########################################
