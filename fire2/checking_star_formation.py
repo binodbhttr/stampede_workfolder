5838ff829a45a07d4502e1951946f94cb19a6924
binodbhttr <binod.bhattarai@hotmail.com>
2021-05-10 00:13:40 -0500

    created plots with other stars born after the cluster detection

--- fire2/checking_star_formation.py
+++ fire2/checking_star_formation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-#this file will load snapshots and store details of the tracked star cluster thoroughout the snapshots into individual files available at ./data
-
 import gizmo_analysis as gizmo
 import utilities as ut
 from matplotlib import pyplot as plt
+import matplotlib
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from sl_utilities import distinct_colours as dc
@@ -13,47 +11,58 @@ from sl_utilities import distance_functions
 import os
 import pickle
 
-
-
 simname = 'm12i_res7100_mhdcv'
 simdir = '/scratch/projects/xsede/GalaxiesOnFIRE/cr_suite/m12i_res7100/mhdcv/1Myr/fire2/'
-
+simtype="fire2"
 snapshot_start=596
 snapshot_end=696 #ran out of memory after 646
-
-
 #Loading the sample cluster to be tracked and sorting its id and id_child
 cluster_group="snapshot596" #Remember to change it if you  are changing the star cluster you are tracking in given snapshot
 
 data_path="./fire2_data_pkl/" #creating a path to store the data only if it does not exist
 gas_datapath="./fire2_gas_data_pkl/"
+young_star_datapath="./fire2_young_stars/"
+young_star_plotpath="./fire2_young_stars_plot/"
 
-###############################################
-###############################################
-#loading data of all clusters (id, id_children and all)
+if not os.path.exists(young_star_datapath):
+  os.makedirs(young_star_datapath)
+
+if not os.path.exists(young_star_plotpath):
+  os.makedirs(young_star_plotpath)
+
+#############################################################################
+#constants
+#############################################################################
+
+MsunToGm = 1.99e33
+KpcToCm = 3.086e21
+mp = 1.67e-24
+#bin_edge = 10.
+bin_edge = 30.
+
+bins = np.arange(-25,25,0.1)
 
 cluster_file_name="clusters_"+simname+"_snapshot_"+str(snapshot_start) 
 with open(data_path+cluster_file_name, "rb") as fp:
     import_cluster = pickle.load(fp)
 
-
-
-
 total_clusters=len(import_cluster)         #count the total no. of clusters we have in clusters file
 total_snaps=snapshot_end-snapshot_start+1  #count the total no. of snapshots we are going to track
 snap=snapshot_start                        #Mark the beginning of the snapshot
-test_cluster=1                             #Mark the beginning of the cluster that we would be tracking
-ind_tracked={}                             #finding the indices of the tracked stars in each snapshot for each cluster.
 
 tracked_stars_each_snap={}     #dictionary for storing data from all clusters and in each snapshot
 
 colors=['cyan','blue','green','magenta','yellow','teal','brown','darkslategray','lime','red','orange','purple','rosybrown','pink','navy','olive','cornflowerblue']
 ##################################
 
+snap=snapshot_start
 for i in range(total_snaps):               #we run a for loop until the end of all snapshots
-  
-  cluster_data_name="all_clusters_at_snapshot_"+str(snapnumber)+".pkl" 
-  with open(datapath+cluster_data_name, "rb") as input:
+  new_stars={}
+  fig1=plt.figure()
+  fig1.set_size_inches(7,7)
+  ax=fig1.add_axes([0.17, 0.185, 0.65, 0.65]) #left, bottom, width, height
+  cluster_data_name="all_clusters_at_snapshot_"+str(snap)+".pkl" 
+  with open(data_path+cluster_data_name, "rb") as input:
     tracked_data= pickle.load(input)
     
   part=gizmo.io.Read.read_snapshots(['star'],'snapshot_index', snap, simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)               #snap is the snapshot number here that changes everytime the loop iterates. It starts with sanpshot_start
@@ -65,65 +74,47 @@ for i in range(total_snaps):               #we run a for loop until the end of a
   Rxy = part['star'].prop('host.distance.principal.cylindrical')[:,0]
   
   cluster_count=0                       #This is the test cluster to begin with. It resets to 1 at each snapshot 
-  tracked_data_all_clusters={}         #This dictionary keeps the tracked information for all star clusters in given snapshot. It also resets with snapshots.
   for j in range(total_clusters):
-    new_stars={}                    #This dictionary holds the tracked information for a cluster temporarily until it is pushed to tracked_data_all_clusters
     xcm=tracked_data[cluster_count+1]["xcm"]
     ycm=tracked_data[cluster_count+1]["ycm"]
-    #circle_radius=((x-xcm)**2+(y-ycm)**2)**(1/2)
-    #region=5
-    #keep_young = np.where((age <= .003) & (circle_radius<=region) & (abs(z) < 1.5))
     x_cluster=tracked_data[cluster_count+1]["x_tracked"]
     y_cluster=tracked_data[cluster_count+1]["y_tracked"]
     ax.scatter(x_cluster,y_cluster,c=colors[j])
+    cluster_count+=1
     
-    test_cluster+=1
-    
-  gas_file_name=simtype+"_gas_data"+str(snapnumber)+".pkl"
+  gas_file_name=simtype+"_gas_data"+str(snap)+".pkl"
   with open(gas_datapath+gas_file_name, "rb") as input:
       import_gasdata = pickle.load(input)
   
-  
-  
-  
+  face=import_gasdata["face"]
+  snaptime=import_gasdata["snaptime"]
+  norm = matplotlib.colors.LogNorm(vmin=1, vmax=1000) #the color range plotted
+  im = ax.imshow(face/(((bins[1]-bins[0])*1000)**2),origin='lower',interpolation='nearest',norm=norm,extent=(-25,25,-25,25),cmap='binary') 
+    
+  #colorbar for the background gas density
+  cmap_gray = matplotlib.cm.get_cmap('binary')
+  norm1 = matplotlib.colors.LogNorm(vmin=1,vmax=1000)
+  cbar_ax1 = fig1.add_axes([0.04, 0.185, 0.04, 0.64]) # position of gray colorbar (left, bottom, width, height)
+  cb1 = fig1.colorbar(im, cax=cbar_ax1, ticklocation='left')
+  cb1.set_label('$\Sigma$ (M$_{{\odot}}$/pc$^2$)', labelpad=-5, fontsize=12)
   
   keep_young = np.where((age <= .003) & (abs(z) < 1.5))
   x0=x[keep_young]
   y0=y[keep_young]
-    
-    
+  
+  ax.scatter(x0,y0,c="red",s=5,label="Young Stars (<3Myr)")
+  ax.set_xlim(np.min(x0)-5,np.max(x0)+5)
+  ax.set_ylim(np.min(y0)-5,np.max(y0)+5)
+  ax.legend(bbox_to_anchor=(1,0.5), loc='center left')
+  
+  fig1.savefig(str(snap)+"test.png",bbox_inches='tight',dpi=200)  
+  fig1.clf()
+  plt.close()  
   ################################################################
   #Now lets write our tracked data from each snapshots to a file
-  new_stars={"ind_tracked":ind_tracked,"age_tracked":age_tracked,"x_tracked":x_tracked,"y_tracked":y_tracked,"z_tracked":z_tracked,
-    "vx_tracked":vx_tracked,"vy_tracked":vy_tracked,"vz_tracked":vz_tracked,
-    "mass_tracked":mass_tracked,"xcm":xcm,"ycm":ycm,"zcm":zcm,"delta_rxyz":delta_rxyz,"rmax":rmax,
-    "ymax":ymax,"ymin":ymin,"xmax":xmax,"xmin":xmin,"avg_delta_rxyz":avg_delta_rxyz,
-    "vR_cyl_tracked":vR_cyl_tracked,"vphi_cyl_tracked":vphi_cyl_tracked,"vz_cyl_tracked":vz_cyl_tracked}
-    
-    
-  tracked_data_all_clusters.update({test_cluster:tracked_data}) #access it using tracked_data_all_clusters[clusterid]["key"]
+  new_stars.update({"x_new_stars":x0,"y_new_stars":y0}) #access it using tracked_data_all_clusters[clusterid]["key"]
   
-  
-  #We have collected all tracked information for our test clusters for one snapshot now which is in dictionary tracked_data_all_clusters.
-  #We would store that dictionary in a pickle file as a backup data from each snapshot about or all star cluster.
-  ###Now moving out of the j loop. We scanned all clusters and stored the tracked data into a dictionary tracked_data_all_clusters  
-  file_name="all_clusters_at_snapshot_"+str(snap)+".pkl"
-  with open(path+file_name, 'wb') as output:
-    pickle.dump(tracked_data_all_clusters, output)
-  print("\n Stored tracked data of all stars clusters in the snapshot no.",snap,"to filename:",file_name,"\n#####\n")
-  #Note we are still in i loop which is scanning each snapshot !!!!!!!!!!!!!!!!!!
-  tracked_data_all_clusters_each_snap.update({snap:tracked_data_all_clusters}) 
-  snap=snap+1
-
-#Came out of i loop 
-  
-#Finally we scanned through all the snapshots and now we come out of the loop to store the final file that contains all the tracked information of all clusters 
-#Now we store the tracked data of all clusters into a dictionary with snapshot number as the key. This would be our final dictionary of dictinaries. 
-with open(path+"total_data_all_clusters_all_snapshots.pkl", 'wb') as output:
-  pickle.dump(tracked_data_all_clusters_each_snap, output) #access the data using tracked_data_all_clusters_each_snap[snapshot][cluster_id]["x_tracked"])
-  
-#Expected Result: tracked_data_all_clusters_each_snap={596:{1:tracked_data,2:tracked_data..},597:{1:tracked_data,2:tracked_data..upto total clusters},598....     upto total snapshots}
-  
-  
-  
-  
+  file_name="young_stars_at_snapshot_"+str(snap)+".pkl"
+  with open(young_star_datapath+file_name, 'wb') as output:
+    pickle.dump(new_stars, output)
+  snap=snap+1 
--- fire2/checking_star_formation_test.py
+++ fire2/checking_star_formation_test.py
@@ -15,7 +15,8 @@ simname = 'm12i_res7100_mhdcv'
 simdir = '/scratch/projects/xsede/GalaxiesOnFIRE/cr_suite/m12i_res7100/mhdcv/1Myr/fire2/'
 simtype="fire2"
 
-snapnumber=650
+snapnumber=686
+snapshot_start=596
 snap=snapnumber
 data_path="./fire2_data_pkl/" 
 gas_datapath="./fire2_gas_data_pkl/"
@@ -60,7 +61,7 @@ im = ax.imshow(face/(((bins[1]-bins[0])*1000)**2),origin='lower',interpolation='
 #colorbar for the background gas density
 cmap_gray = matplotlib.cm.get_cmap('binary')
 norm1 = matplotlib.colors.LogNorm(vmin=1,vmax=1000)
-cbar_ax1 = fig1.add_axes([0.04, 0.185, 0.04, 0.64]) # position of gray colorbar (left, bottom, width, height)
+cbar_ax1 = fig1.add_axes([0.09, 0.185, 0.03, 0.65]) # position of gray colorbar (left, bottom, width, height)
 cb1 = fig1.colorbar(im, cax=cbar_ax1, ticklocation='left')
 cb1.set_label('$\Sigma$ (M$_{{\odot}}$/pc$^2$)', labelpad=-5, fontsize=12)
 #plot a scale bar 5 kpc long
@@ -77,11 +78,8 @@ label3 = 'time = ' + f'{snaptime:.3f}' + ' Gyr'
 #ax.text(-23.5,-21,label3,fontsize=13.5) #display time on the bottom left above simtype
 
 
-x=tracked_data[cluster_group]["x_tracked"]
-y=tracked_data[cluster_group]["y_tracked"]
-
-ax.scatter(x,y,c="cornflowerblue",label="clusters 17")
-
+x_tracked=tracked_data[cluster_group]["x_tracked"]
+y_tracked=tracked_data[cluster_group]["y_tracked"]
 
 #################################################Looking for young stars in the region###################
 part=gizmo.io.Read.read_snapshots(['star'],'snapshot_index', snap, simulation_name=simname, simulation_directory=simdir, assign_hosts=True, assign_hosts_rotation=True)               #snap is the snapshot number here that changes everytime the loop iterates. It starts with sanpshot_start
@@ -105,14 +103,28 @@ keep = np.where((age <= .003) & (circle_radius<=region) & (abs(z) < 1.5))
 x0=x[keep]
 y0=y[keep]
 
-ax.scatter(x0,y0,c="red",s=5,label="Young Stars (<3Myr)")
+#keep_old=np.where((age <= .060) & (age > 0.003) & (circle_radius<=region) & (abs(z) < 1.5))
+time=(snapnumber-snapshot_start)/1000
+keep_old=np.where((age <= time) & (circle_radius<=region) & (abs(z) < 1.5))
+x0_old=x[keep_old]
+y0_old=y[keep_old]
+age_old=age[keep_old]
+#ax.scatter(x0_old,y0_old,c="red",s=7,label="Old Stars (<60Myr)")
+#ax.scatter(x0,y0,c="blue",label="Young Stars (<3Myr)")
+age_old=1000*age_old #Converting to Myr
+cb2=ax.scatter(x0_old,y0_old,c=age_old,s=7,cmap=plt.cm.get_cmap('bwr'), vmin=np.min(age_old),vmax=np.max(age_old))
+cbar_ax = fig1.add_axes([0.81, 0.185, 0.03, 0.65]) # position of the colorbar (left, bottom, width, height)
+fig1.colorbar(cb2, cax=cbar_ax)
+cbar_ax.set_ylabel('Age(Myr)')
+cbar_ax.yaxis.label.set_size(12)
 
+ax.scatter(x_tracked,y_tracked,c="green",s=100,marker="*",label="clusters 17",edgecolor='black')#cluster 17 is cornflowerblue
 circle=plt.Circle((xcm,ycm),region,fill=False)
 ax.add_patch(circle)
 
 ax.set_xlim(np.min(x0)-5,np.max(x0)+5)
 ax.set_ylim(np.min(y0)-5,np.max(y0)+5)
 
-ax.legend(bbox_to_anchor=(1,0.5), loc='center left')
-
+#ax.legend(bbox_to_anchor=(1,0.5), loc='center left')
+ax.set_title("time= "+str(snapnumber-snapshot_start)+" Myr")
 fig1.savefig(str(snapnumber)+"test.png",bbox_inches='tight',dpi=200)
